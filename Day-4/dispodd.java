3ac2503355b906471b2962b008b6cdaf214b970f
Shadow_supreme <67516832+Shadow-supreme22@users.noreply.github.com>
2020-10-14 11:08:18 +0530

    Add files via upload

---
+++ Day-4/dispodd.java
@@ -0,0 +1,16 @@
+//Done BY Varinderjot 
+//Que-2
+package dispodd;
+
+public class dispodd {
+	 public static void main(String[] args) {
+	        int[] array = {1, 2, 3, 4, 5};
+	        for(int i=0;i<array.length;i++){  
+	        	if(array[i]%2!=0){  
+	        		System.out.println(array[i]);
+	        	}
+	        }
+	        	        
+	        
+	    }
+}
