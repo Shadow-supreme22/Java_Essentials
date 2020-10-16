8934cce6a202f8708024e8b9bc911cd558d0add5
Shadow5supreme <shadowvj3@gmail.com>
2020-10-16 20:29:50 +0530

    Update2

--- Day-5/quizapp/Game.java
+++ Day-5/quizapp/Game.java
@@ -18,27 +18,6 @@ public class Game {
         for(int i=0;i<3;i++){
             questions[i]=new Question();
         }
-//        applying values to the variables of the object
-//        questions[0].question="Who is the Strongest Avenger?";
-//        questions[0].option1="";
-//        questions[0].option2="Thor";
-//        questions[0].option3="Hulk";
-//        questions[0].option4="Dr Strange";
-//        questions[0].correctAnswer=2;
-//
-//        questions[1].question="";
-//        questions[1].option1="Earth";
-//        questions[1].option2="Venus";
-//        questions[1].option3="Mercury";
-//        questions[1].option4="Jupiter";
-//        questions[1].correctAnswer=3;
-//
-//        questions[2].question="";
-//        questions[2].option1="Sydney";
-//        questions[2].option2="Melbourne";
-//        questions[2].option3="Perth";
-//        questions[2].option4="Canberra";
-//        questions[2].correctAnswer=4;
 
         for(int i=0;i<3;i++)
         {
